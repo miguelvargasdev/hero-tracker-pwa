c405c8fabba3c3b8c05af42e9734fb7201116f8d
Miguel Vargas <contact@miguelvargas.dev>
2025-01-19 12:29:40 -0500

    add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 import manifest from './manifest.json';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/hero-tracker-pwa/',
   plugins: [
