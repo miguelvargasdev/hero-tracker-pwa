d19b1a7f6765c848f952dcf41acc7255800be89c
Miguel Vargas <contact@miguelvargas.dev>
2025-01-19 11:58:11 -0500

    manifest changes

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,10 +1,10 @@
 import Meta from '@/components/Meta';
 import { FullSizeCenteredFlexBox } from '@/components/styled';
-import useOrientation from '@/hooks/useOrientation';
+// import useOrientation from '@/hooks/useOrientation';
 import Button from '@mui/material/Button';
 
 function Welcome() {
-  const isPortrait = useOrientation();
+  // const isPortrait = useOrientation();
 
   // const width = isPortrait ? '40%' : '30%';
   // const height = isPortrait ? '30%' : '40%';
@@ -12,7 +12,7 @@ function Welcome() {
   return (
     <>
       <Meta title="Welcome" />
-      <FullSizeCenteredFlexBox flexDirection={isPortrait ? 'column' : 'row'}>
+      <FullSizeCenteredFlexBox flexDirection={'column'} gap={2}>
         <Button variant="contained">Solo</Button>
         <Button variant="contained">Standard</Button>
         <Button variant="contained">Tyrant</Button>
